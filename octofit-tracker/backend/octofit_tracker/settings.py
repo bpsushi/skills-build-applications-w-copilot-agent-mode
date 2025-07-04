3cd85005cde9344ed20dc5302125f0380f430645
Bartłomiej Ponikiewski <bpsushi@users.noreply.github.com>
2025-07-04 10:37:24 +0000

    Update ALLOWED_HOSTS in settings.py and modify api_root view to use base URL for API endpoints

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-7t&8&_m_j6hcwz3w(x*9xyupoawnj(2-o42k#5%t2%wht1yn4#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev']
 
 
 # Application definition
--- octofit-tracker/backend/octofit_tracker/views.py
+++ octofit-tracker/backend/octofit_tracker/views.py
@@ -26,10 +26,11 @@ class WorkoutViewSet(viewsets.ModelViewSet):
 
 @api_view(['GET'])
 def api_root(request, format=None):
+    base_url = 'https://fluffy-space-halibut-7jxpj954pj2jp-8000.app.github.dev/'
     return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'teams': reverse('team-list', request=request, format=format),
-        'activity': reverse('activity-list', request=request, format=format),
-        'leaderboard': reverse('leaderboard-list', request=request, format=format),
-        'workouts': reverse('workout-list', request=request, format=format),
+        'users': base_url + 'api/users/',
+        'teams': base_url + 'api/teams/',
+        'activity': base_url + 'api/activity/',
+        'leaderboard': base_url + 'api/leaderboard/',
+        'workouts': base_url + 'api/workouts/',
     })
